90a0a9c02951c59c0c6df51c07b4d8c5f1aa9ab7
laokuang <170819739@qq.com>
2017-12-27 16:42:06 +0800

    测试项目demo init

---
+++ src/com/kaishengit/demo/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.kaishengit.demo;
+
+/**
+ * @author kuang
+ * @create 2017-12-27
+ */
+public class HelloWorld {
+
+
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+
+}
